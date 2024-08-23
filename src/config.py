a6b742caeb64bd289a20106e5870854dfdc4a32a
Tomáš Janda <tomas.janda@creativedock.com>
2024-08-23 20:46:45 +0200

    add tickers to config

--- src/config.py
+++ src/config.py
@@ -24,3 +24,20 @@ SERVICES = {
 
 FINANCIAL_DATA_SCREENER_FILTERS = 'cap_microover,fa_debteq_u1,fa_roa_pos,geo_usa'
 FINANCIAL_DATA_SCREENER_ORDER = '-roa'
+
+FRED_API_KEY = os.environ.get('FRED_API_KEY')
+
+MACRO_YDATA_TICKERS = {
+    'S&P 500': '^GSPC',
+    'VIX': '^VIX',
+    '10Y Treasury Yield': '^TNX',
+    'Gold': 'GC=F',
+    'Oil (WTI Crude)': 'CL=F',
+    'U.S. Dollar Index': 'DX-Y.NYB'
+}
+
+FRED_MACRO_DATA = {
+    'M2 Money Supply': 'M2SL',
+    'Consumer Sentiment Index': 'UMCSENT',
+    'Industrial Production Index': 'INDPRO'
+}
